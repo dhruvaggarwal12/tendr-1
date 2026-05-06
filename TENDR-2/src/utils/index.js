3218d017e294297cc813686540047194659537aa
dhruvaggarwal12 <dhruvaggarwal1144@gmail.com>
2026-05-07 02:26:16 +0530

    chore: remove TENDR-2 from tracking — backend has its own repo

--- TENDR-2/src/utils/index.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  ...require('./date'),
-  ...require('./cache'),
-  ...require('./pagination'),
-  ...require('./location'),
-  ...require('./response'),
-  ...require('./analyticsHelpers')
-}; 
