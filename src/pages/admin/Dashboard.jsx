2298eb0371d995a4643cb945a11704a654a499fb
dhruvaggarwal12 <dhruvaggarwal1144@gmail.com>
2026-06-30 00:36:35 +0530

    fix: add React default import for EbooksAdminTab (React is not defined)

--- src/pages/admin/Dashboard.jsx
+++ src/pages/admin/Dashboard.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { generateEventDetailsPDF, generateInvoicePDF, generateInvitationPDF, generateTimelinePDF } from "../../utils/pdfGenerator";
